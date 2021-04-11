1f986fd487fbf6ded14c946cbc180967237e462b
girvel <widauka@ya.ru>
2021-04-11 19:27:09 +0500

    Initial; realized v0.1 prototype

---
+++ parser.py
@@ -0,0 +1,28 @@
+import parsimonious as pm
+import ast
+
+with open('grammar.txt') as f:
+    grammar = pm.Grammar(f.read())
+
+with open('example.txt') as f:
+    syntax_tree = grammar.parse(f.read())
+
+
+class Translator(pm.NodeVisitor):
+    def visit_expression(self, node, children):
+        return "\n".join((children[0], *(pair[1] for pair in children[1])))
+        
+    def visit_cpp_call(self, node, children):
+        _, string, _ = children
+        return ast.literal_eval(string)
+
+    def visit_string_literal(self, node, children):
+        return node.text
+
+    def generic_visit(self, node, visited_children):
+        return visited_children or None
+
+with open('template.cpp') as f:
+    template = f.read()
+
+result = template.format(Translator().visit(syntax_tree))
